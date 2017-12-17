823728150da611f5a11985fc76475b0a9542f9fe
z00nx 0 <z00nx0@gmail.com>
2017-12-18 10:30:56 +1100

    Added de4dot

---
+++ de4dot/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'de4dot'
+$url = ''
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName            = $packageName
+  url                    = ''
+  checksum               = ''
+  checksumType           = ''
+  unzipLocation          = $unzipLocation
+}
+
+Install-ChocolateyZipPackage @packageArgs
