6daeff72e96bdf7957cd3175e53d98accb1d7735
z00nx 0 <z00nx0@gmail.com>
2018-03-13 14:34:20 +0000

    AU: 1 updated - ilspy [skip ci]

--- ilspy/tools/chocolateyInstall.ps1
+++ ilspy/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ $toolsDir = Join-Path $toolsDir $packageName
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/icsharpcode/ILSpy/releases/download/v3.0.1/ILSpy_binaries_Net46_Win_3.0.1.3459.zip'
-  checksum      = '5aca34b25fa416cc76af3a98d663791d951fdd6455f2d8b4e9e4520a315e4346'
+  url           = 'https://github.com/icsharpcode/ILSpy/releases/download/v3.1-beta1/ILSpy_binaries_Net46_Win_3.1.0.3599-beta1.zip'
+  checksum      = '3f486a9fb33252fb2d23461795df932f6927c1b9c3cba1f57fe8058c3e6105a3'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
