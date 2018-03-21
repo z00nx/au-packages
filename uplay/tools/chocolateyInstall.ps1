82f1f74b8fbf72c51ec811f67a2b6a52f0187b86
z00nx 0 <z00nx0@gmail.com>
2018-03-21 21:55:10 +1100

    Added uplay

---
+++ uplay/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = 'http://ubi.li/4vxt9'
+  softwareName   = 'Uplay'
+  checksum       = '134AC093E85C5E0B293D9D43F468F8FF8CDE41D5228574583168808017D3FA574A0EED2B0C828D38DF349C22F1B513B7E6C55D7EAE890D29BF287A4B3BAB2060'
+  checksumType   = 'sha512'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
