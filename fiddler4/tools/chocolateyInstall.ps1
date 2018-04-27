34e9337ec199151565ae6e475d207cd3ebd76058
z00nx 0 <z00nx0@gmail.com>
2018-04-27 02:46:21 +0000

    Added installation directory

--- fiddler4/tools/chocolateyInstall.ps1
+++ fiddler4/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url                    = 'https://www.telerik.com/docs/default-source/fiddler/fiddlersetup.exe'
   checksum               = '09CF05F9F805AA2114CC9373A10EF882611BD513E2C461CDBA49F35BDCAE1CFD'
   checksumType           = 'sha256'
-  silentArgs             = '/S'
+  silentArgs             = '/S /D=C:\Program Files\Fiddler'
   validExitCodes         = @(0)
   softwareName           = 'fiddler*'
 }
