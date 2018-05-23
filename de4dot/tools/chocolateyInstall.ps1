57d5c603321275030832dadfc4db48fab65df0a5
z00nx 0 <z00nx0@gmail.com>
2018-05-23 14:33:18 +0000

    AU: 2 updated - de4dot wireshark [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/14f4ebf7ccb739b96a241710fbb7742833ada187

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = $packageName
-  url                    = 'https://ci.appveyor.com/api/buildjobs/1fqpwv8oy3l3gd1u/artifacts/de4dot.zip'
-  checksum               = '4b57c4e0cf47365f59a07342ed5a6aea6231f3f4b6aba567d89ca3d0c5974b09'
+  url                    = 'https://ci.appveyor.com/api/buildjobs/6s1s4f2a4171yn5k/artifacts/de4dot.zip'
+  checksum               = '09e300166f7a86eb2b20a984245781b5ce30509a82bc34bc2f7afbe0e537aef2'
   checksumType           = 'sha256'
   unzipLocation          = $unzipLocation
 }
--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'WireShark'
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
-  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-2.6.0.exe'
-  checksum               = 'd96e76ab9d5d94cb70cd7686ff0302d482a5be79ae8fc95934902de79b9be94b'
+  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-2.6.1.exe'
+  checksum               = 'f1e2a07e696338217671b58076073ad3a8ae165ece7fcb71866b6dcc750e1bf8'
   checksumType           = 'sha256'
-  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-2.6.0.exe'
-  checksum64             = 'a5c276cd3b2b3023b597debdf292cccb7f7f64400cd40d2ef5dd139d18424936'
+  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-2.6.1.exe'
+  checksum64             = '7fea0e6edf6e6a10d277a354fc9982c6d15c2f2aa05ec13c714e077b8c8760a9'
   checksumType64         = 'sha256'
   silentArgs             = '/S /quicklaunchicon=no'
   validExitCodes         = @(0)
