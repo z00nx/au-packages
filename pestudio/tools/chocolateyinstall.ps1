54cb9bd932fc6a0cf5404f0dcd501cd352c937c5
z00nx 0 <z00nx0@gmail.com>
2018-06-02 14:33:30 +0000

    AU: 1 updated - pestudio [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/000853fecc2cc5798eb7c7bfa646dae5ffa1bd1d

--- pestudio/tools/chocolateyinstall.ps1
+++ pestudio/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url           = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
-  checksum      = 'fba2f8fe54671f687491d0db9685e961b1fecfdc74b5006946d4477a2ff403c6'
+  checksum      = 'f01e0f93b2993cfa3feb057fb8e99042d7f094f8a7c0eb3f201ebb0ea5edc61d'
   checksumType  = 'sha256'
 }
 
