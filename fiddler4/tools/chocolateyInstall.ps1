8c8bf70ef437924ccc9835ffecc48f8e5029bdc3
z00nx 0 <z00nx0@gmail.com>
2018-06-28 14:50:56 +0000

    AU: 2 updated - fiddler4 uplay [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/1c7b273f539261f2bca120c828b996324315d370

--- fiddler4/tools/chocolateyInstall.ps1
+++ fiddler4/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName            = 'fiddler4'
   fileType               = 'EXE'
   url                    = 'https://www.telerik.com/docs/default-source/fiddler/fiddlersetup.exe'
-  checksum               = '09CF05F9F805AA2114CC9373A10EF882611BD513E2C461CDBA49F35BDCAE1CFD'
+  checksum               = 'DD9C48E2C37124477320DDEF083DD0FAFCB1B59E0ADE9E881D85FC80EBB82351'
   checksumType           = 'sha256'
   silentArgs             = '/S /D=C:\Program Files\Fiddler'
   validExitCodes         = @(0)
