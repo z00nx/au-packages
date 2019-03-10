5b6f6f246e5e727008639c670d8e29923fcf6fa2
z00nx 0 <z00nx0@gmail.com>
2019-03-10 14:50:43 +0000

    AU: 3 updated - de4dot uplay wireshark [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/b204a25dd6b4414e491b6304961c0b4e5c7ac5f6

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = $packageName
-  url                    = 'https://ci.appveyor.com/api/buildjobs/4xvhj2hlneh1u1je/artifacts/Release/de4dot-net35.zip'
-  checksum               = '03c64a8c65fe4259378bc378f37879c702cb75e700752f2d503f10ca79e26c35'
+  url                    = 'https://ci.appveyor.com/api/buildjobs/galkqpugmv5h2hjw/artifacts/Release/de4dot-net35.zip'
+  checksum               = 'd947ecf5498f794d8ada462f28662c552465b58b1f9c52af0bf8cac037258731'
   checksumType           = 'sha256'
   unzipLocation          = $unzipLocation
 }
--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'WireShark'
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
-  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-2.6.6.exe'
-  checksum               = 'a76e70694b1914c3bf2c5267c234d44db58a605b88c274473368cb57b4262c1f'
+  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-3.0.0.exe'
+  checksum               = 'ca59a35866b5837578070ae064bb099d7a50b81be6b86884679aa86ef08ea89f'
   checksumType           = 'sha256'
-  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-2.6.6.exe'
-  checksum64             = 'e440a059fba3572000b5695ca89438d5765b9d73d830556344d7e8cdad071617'
+  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.0.0.exe'
+  checksum64             = 'b5d30a9c0b2835a41e8132b1d9246a01ad6f1461a4de14fa3ccbdf4919c70eac'
   checksumType64         = 'sha256'
   silentArgs             = '/S /quicklaunchicon=no'
   validExitCodes         = @(0)
