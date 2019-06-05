1db6b835d9db0c7151015c7ad0165bdeebcfdf37
z00nx 0 <z00nx0@gmail.com>
2019-06-05 14:50:32 +0000

    AU: 1 updated - fiddler4 [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/055b57d05c8d05d892873c39422f456ca011ee8a

--- fiddler4/tools/chocolateyInstall.ps1
+++ fiddler4/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName            = 'fiddler4'
   fileType               = 'EXE'
   url                    = 'https://www.telerik.com/docs/default-source/fiddler/fiddlersetup.exe'
-  checksum               = 'DD9C48E2C37124477320DDEF083DD0FAFCB1B59E0ADE9E881D85FC80EBB82351'
+  checksum               = '4DFF6CB1B88A503D3982C7CCBCD5622496EBA4C21AAB5A2C0C0F1E376A2EB809'
   checksumType           = 'sha256'
   silentArgs             = '/S /D=C:\Program Files\Fiddler'
   validExitCodes         = @(0)
