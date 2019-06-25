3ecbbfd751e3dad4f885828ea73ace1cd33e7c17
z00nx 0 <z00nx0@gmail.com>
2019-06-25 14:50:35 +0000

    AU: 2 updated - de4dot uplay [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/94f79e8268e3574c6e02e17b35613dbd73bc6cf9

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = $packageName
-  url                    = 'https://ci.appveyor.com/api/buildjobs/5vwp9iw3dw7n01pn/artifacts/Release/de4dot-net35.zip'
-  checksum               = 'afb815f971e13b93e8cb7255a0cec5805dcae4b1acb3fda886fdc56b250b3f02'
+  url                    = 'https://ci.appveyor.com/api/buildjobs/dl4qvl4lo1krqtrp/artifacts/Release/de4dot-net35.zip'
+  checksum               = 'e398e54a20d9a4aba71a23c40054059a97d0b4360f5e9dfb94ec402733746bea'
   checksumType           = 'sha256'
   unzipLocation          = $unzipLocation
 }
