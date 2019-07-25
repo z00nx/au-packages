c6fb029fd767d42a44391c5a2dc0aafb89f4b250
z00nx 0 <z00nx0@gmail.com>
2019-07-25 14:50:34 +0000

    AU: 2 updated - uplay wireshark [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/385bbe3ea1f946ccb09e282235670c67d1e18728

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'WireShark'
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
-  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-3.0.2.exe'
-  checksum               = 'ea4f9a4b55bed282408d595bf2e86951014e108144662f8add51cb0a7ad034ed'
+  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-3.0.3.exe'
+  checksum               = '9e276aff3164e98293d478758542855bdfb7ffd295156f4d5011ba76a9cbf814'
   checksumType           = 'sha256'
-  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.0.2.exe'
-  checksum64             = '27d15f43091e002d454fb63f8c438fe4b32a86b57c02c6cb83ef36bdbfaefe09'
+  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.0.3.exe'
+  checksum64             = '824cbeeb48ed720a24b70725cb8db2353ed331e05f3d0a832ff96acb02292d62'
   checksumType64         = 'sha256'
   silentArgs             = '/S /quicklaunchicon=no'
   validExitCodes         = @(0)
