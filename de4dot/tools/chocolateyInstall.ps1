e2d64c304650c355a7bef9f0270d3c06b3854d0e
z00nx 0 <z00nx0@gmail.com>
2019-09-04 14:50:47 +0000

    AU: 1 updated - de4dot [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/6390916b5c592c8c6ac964fadac228be4c478fde

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = $packageName
-  url                    = 'https://ci.appveyor.com/api/buildjobs/dl4qvl4lo1krqtrp/artifacts/Release/de4dot-net35.zip'
-  checksum               = 'e398e54a20d9a4aba71a23c40054059a97d0b4360f5e9dfb94ec402733746bea'
+  url                    = 'https://ci.appveyor.com/api/buildjobs/r9yopsfgp3ub9p8v/artifacts/Release/de4dot-net35.zip'
+  checksum               = '656eaf5d84e4b46717f85f111e5313e3997c6635e648178aac8285c26c41eee3'
   checksumType           = 'sha256'
   unzipLocation          = $unzipLocation
 }
