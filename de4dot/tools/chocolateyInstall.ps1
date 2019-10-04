a595fbbed51d0a2baff2b8a5b4e8bd9e18eb7b0e
z00nx 0 <z00nx0@gmail.com>
2019-10-04 14:50:33 +0000

    AU: 1 updated - de4dot [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/a9943db3fd62b738e58e4ecfc40e1c033153674e

--- de4dot/tools/chocolateyInstall.ps1
+++ de4dot/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = $packageName
-  url                    = 'https://ci.appveyor.com/api/buildjobs/8s7ddxrm6f35m6se/artifacts/de4dot-netcoreapp3.0.zip'
-  checksum               = '1a02879b809da42f4d4a14a2418b3de1b5af78b2f88c49b769665a7ced465790'
+  url                    = 'https://ci.appveyor.com/api/buildjobs/9iliec2gacwvxy20/artifacts/de4dot-netcoreapp3.0.zip'
+  checksum               = '070bdfe1066a359bdaa2203dbc02eae7c0e501208c0f525c8d15cc4edf1f5053'
   checksumType           = 'sha256'
   unzipLocation          = $unzipLocation
 }
