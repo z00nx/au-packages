e4990cb22fc598daca3feba878522c5ccee0dd6c
z00nx 0 <z00nx0@gmail.com>
2020-08-13 14:50:36 +0000

    AU: 1 updated - wireshark [skip ci] https://gist.github.com/659441c4a5f80ceae7a71ff3eb524d17/fdf57d9d47ac98c20991798704e31460485a45f0

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = 'WireShark'
 $packageArgs = @{
   packageName            = $packageName
   fileType               = 'EXE'
-  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-3.2.5.exe'
-  checksum               = '728d51ba8e1d551ff29b2432933923112bc5d43100ee4b327085dbdda739dcd1'
+  url                    = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-3.2.6.exe'
+  checksum               = '1b0b2a495878710417bb49640971d8188039c60d669a5ca8cfa273b027274d49'
   checksumType           = 'sha256'
-  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.2.5.exe'
-  checksum64             = 'b9626086253e00fbaf35c7d91c768aa4895d0d2e59b4e48b01331dce7cfeb510'
+  url64                  = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-3.2.6.exe'
+  checksum64             = '9c211a6eda5f69b2c10dd1a794f8a438c1dafbe5da4879b807be496d8f29a6c4'
   checksumType64         = 'sha256'
   silentArgs             = '/S /quicklaunchicon=no'
   validExitCodes         = @(0)
